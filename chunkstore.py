2eaa593de8e53b8191fcc376f07f64f324a706fa
Andrew Vineyard <dimensional42@gmail.com>
2025-01-31 11:12:38 -0600

    chunkstore: Added more functions/methods, and created variable for use of grouping chunks by index for write_csm.

--- chunkstore.py
+++ chunkstore.py
@@ -6,7 +6,7 @@ from sys import argv
 import glob
 
 class Chunkstore:
-    def __init__(self, filename, depot=None, is_encrypted=False, max_file_size=1 * 1024 * 1024 * 1024):
+    def __init__(self, filename, depot=None, is_encrypted=None, max_file_size=1 * 1024 * 1024 * 1024):
         filename = filename.replace(".csd", "").replace(".csm", "")
         self.base_filename = filename
         self.depot = depot
@@ -15,6 +15,7 @@ class Chunkstore:
         self.file_index = 1
         self.chunks = {}
         self._load_existing_files()
+        self.chunks_by_csm = {}  # New variable to store chunks by CSM index
 
     def _load_existing_files(self):
         csm_files = sorted(glob.glob(f"{self.base_filename}_*.csm"))
@@ -30,7 +31,7 @@ class Chunkstore:
                 self.csm = csmfile.read()
                 if self.csm[:4] != b"SCFS":
                     print("Not a CSM file: " + self.csmname)
-                    return False
+                    raise Exception(f"Not a CSM file: {self.csmname}")
                 self.depot = int.from_bytes(self.csm[0xc:0x10], byteorder='little', signed=False)
                 self.is_encrypted = (self.csm[0x8:0xa] == b'\x03\x00')
                 self._unpack()
@@ -38,7 +39,7 @@ class Chunkstore:
             self.file_index += 1
         self.file_index -= 1
         if self.depot is None or self.is_encrypted is None:
-            raise Exception("Need to specify depot and encryption if file doesn't already exist")
+            raise Exception("Need to specify depot or encryption if file doesn't already exist")
 
     def __repr__(self):
         return f"Depot {self.depot} (encrypted: {self.is_encrypted}, chunks: {len(self.chunks)}) from CSD file {self.csdname}"
@@ -51,20 +52,31 @@ class Chunkstore:
             self.chunks[sha] = (offset, length, self.file_index)
             if unpacker:
                 unpacker(self, sha, offset, length)
+            # Add chunk to chunks_by_csm
+            if self.file_index not in self.chunks_by_csm:
+                self.chunks_by_csm[self.file_index] = []
+            self.chunks_by_csm[self.file_index].append((sha, offset, length))
 
     def write_csm(self):
         with open(self.csmname, "wb") as csmfile:
             csmfile.write(b"SCFS\x14\x00\x00\x00")
-            if self.is_encrypted:
-                csmfile.write(b"\x03\x00\x00\x00")
-            else:
-                csmfile.write(b"\x02\x00\x00\x00")
+            # if self.is_encrypted:
+            #     csmfile.write(b"\x03\x00\x00\x00")
+            # else:
+            #     csmfile.write(b"\x02\x00\x00\x00")
+            ## Same as above, but made single line.
+            csmfile.write(b"\x03\x00\x00\x00" if self.is_encrypted else b"\x02\x00\x00\x00")
             csmfile.write(pack("<L L", self.depot, len(self.chunks)))
             csmfile.seek(0, 2)
-            for sha, (offset, length, file_index) in self.chunks.items():
-                if file_index == self.file_index:
-                    csmfile.write(sha)
-                    csmfile.write(pack("<Q L L", offset, 0, length))
+            # Remove the redundant for loop
+            # for sha, (offset, length, file_index) in self.chunks.items():
+            #     if file_index == self.file_index:
+            #         csmfile.write(sha)
+            #         csmfile.write(pack("<Q L L", offset, 0, length))
+            # Write chunks in order of offset using chunks_by_csm
+            for sha, offset, length in sorted(self.chunks_by_csm[self.file_index], key=lambda x: x[1]):
+                csmfile.write(sha)
+                csmfile.write(pack("<Q L L", offset, 0, length))
 
     def get_chunk(self, sha):
         offset, length, file_index = self.chunks[sha]
@@ -82,8 +94,8 @@ class Chunkstore:
                 length = len(data)
                 if offset + length > self.max_file_size:
                     self.write_csm()
-                    self.chunks = {}  # Clear the chunks dictionary
                     self.file_index += 1
+                    self.chunks = {}  # Clear the chunks dictionary
                     self.csmname = f"{self.base_filename}_{self.file_index}.csm"
                     self.csdname = f"{self.base_filename}_{self.file_index}.csd"
                     makedirs(path.dirname(self.csdname), exist_ok=True)
@@ -91,15 +103,32 @@ class Chunkstore:
                 else:
                     csdfile.write(data)
                     self.chunks[sha] = (offset, length, self.file_index)
+                    # Add chunk to chunks_by_csm
+                    if self.file_index not in self.chunks_by_csm:
+                        self.chunks_by_csm[self.file_index] = []
+                    self.chunks_by_csm[self.file_index].append((sha, offset, length))
         else:
             makedirs(path.dirname(csdname), exist_ok=True)
             with open(csdname, "wb") as csdfile:
                 offset = 0
                 length = csdfile.write(data)
                 self.chunks[sha] = (offset, length, self.file_index)
+                # Add chunk to chunks_by_csm
+                if self.file_index not in self.chunks_by_csm:
+                    self.chunks_by_csm[self.file_index] = []
+                self.chunks_by_csm[self.file_index].append((sha, offset, length))
 
     def get_all_chunks(self):
         return self.chunks
+
+    def get_last_index(self):
+        csm_files = sorted(glob.glob(f"{self.base_filename}_*.csm"))
+        return len(csm_files)
+
+    def set_current_index(self, index):
+        self.file_index = index
+        self.csmname = f"{self.base_filename}_{self.file_index}.csm"
+        self.csdname = f"{self.base_filename}_{self.file_index}.csd"
 
 if __name__ == "__main__":
     if len(argv) > 1:
